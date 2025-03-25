6cfb6246584ec62ed5273810114c74659337c36e
aimodels-trust <sdpfinalbusiness@gmail.com>
2025-03-25 18:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Credit Default Prediction", layout="wide")
 
 # Step 1: Download the model from Google Drive
-model_url = "https://drive.google.com/uc?id=1en2IPj_z6OivZCBNDXepX-EAiZLvCILE"
+model_url = "https://drive.google.com/uc?id=1b_7MNUEJjOvhq-eLSH3gu1scneUB5At9"
+
 model_path = "credit_default_model.pkl"
 
 if not os.path.exists(model_path):
